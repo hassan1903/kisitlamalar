7fb4a25b5b609c7fee7298bb665c4e28a15b6a51
Hasan Kürşat Küçüköztaş <hasan.kucukoztas@maarifa.com.tr>
2021-03-02 01:45:54 +0300

    fix

---
+++ src/index.js
@@ -0,0 +1,93 @@
+// @flow
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Select from "react-select";
+import cityList from "./city.json";
+import ruleList from "./rules.json";
+import "./styles.css";
+
+function LightBulb() {
+  let [light, setLight] = useState(0);
+  let [selectedCity, setSelectedCity] = useState({ value: "", label: "" });
+  let ruleId = 1;
+  const colorCode = [
+    {
+      key: 1,
+      value: "#3363ff"
+    },
+    { key: 2, value: "#ffbb73" },
+    { key: 3, value: "#ff7f00" },
+    { key: 4, value: "#ff3933" }
+  ];
+  const fillColor = colorCode.find((element) => element.key === light);
+  return (
+    <div className="App">
+      <div style={{ paddingBottom: 50, textAlign: "center" }}>
+        <LightbulbSvg fillColor={fillColor?.value || "#000"} />
+      </div>
+      <Select
+        placeholder="Şehir seçiniz"
+        value={selectedCity}
+        options={cityList.cities}
+        onChange={(val) => {
+          const cityStatus = cityList.cities.find(
+            (item) => item.label === val.label
+          );
+          setLight(cityStatus.status);
+          setSelectedCity(val);
+        }}
+      />
+      {light !== 0 ? (
+        <ul>
+          {ruleList.rules.map((el) => {
+            if (el.ruleId === ruleId) {
+              const index = el.status.indexOf(light);
+              if (index > -1) {
+                ruleId++;
+                return <li key={ruleId}>{el.label}</li>;
+              }
+            }
+          })}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
+function LightbulbSvg(props) {
+  return (
+    <svg width="56px" height="90px" viewBox="0 0 56 90" version="1.1">
+      <defs />
+      <g
+        id="Page-1"
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+      >
+        <g id="noun_bulb_1912567" fill="#000000" fillRule="nonzero">
+          <path
+            d="M38.985,68.873 L17.015,68.873 C15.615,68.873 14.48,70.009 14.48,71.409 C14.48,72.809 15.615,73.944 17.015,73.944 L38.986,73.944 C40.386,73.944 41.521,72.809 41.521,71.409 C41.521,70.009 40.386,68.873 38.985,68.873 Z"
+            id="Shape"
+          />
+          <path
+            d="M41.521,78.592 C41.521,77.192 40.386,76.057 38.986,76.057 L17.015,76.057 C15.615,76.057 14.48,77.192 14.48,78.592 C14.48,79.993 15.615,81.128 17.015,81.128 L38.986,81.128 C40.386,81.127 41.521,79.993 41.521,78.592 Z"
+            id="Shape"
+          />
+          <path
+            d="M18.282,83.24 C17.114,83.24 16.793,83.952 17.559,84.83 L21.806,89.682 C21.961,89.858 22.273,90 22.508,90 L33.492,90 C33.726,90 34.039,89.858 34.193,89.682 L38.44,84.83 C39.207,83.952 38.885,83.24 37.717,83.24 L18.282,83.24 Z"
+            id="Shape"
+          />
+          <path
+            d="M16.857,66.322 L39.142,66.322 C40.541,66.322 41.784,65.19 42.04,63.814 C44.63,49.959 55.886,41.575 55.886,27.887 C55.887,12.485 43.401,0 28,0 C12.599,0 0.113,12.485 0.113,27.887 C0.113,41.575 11.369,49.958 13.959,63.814 C14.216,65.19 15.458,66.322 16.857,66.322 Z"
+            id="Shape"
+            fill={props.fillColor}
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<LightBulb />, rootElement);
