95ecd73bdedfe6eeb8a3953794dc46e6c2bfdffc
Hasan Kürşat Küçüköztaş <hasan.kucukoztas@maarifa.com.tr>
2021-03-02 22:30:52 +0300

    add twitter

--- src/index.js
+++ src/index.js
@@ -85,16 +85,23 @@ const LightBulb = () => {
           </ul>
         ) : null}
         {light !== 0 && selectedCity ? (
-          <h6 className="title">
-            {
-              "Bilgilendirme amaçlıdır, gerçeği yansıtmayabilir, sorumluluk kabul edilmez."
-            }
-          </h6>
-        ) : null}
-        {light !== 0 && selectedCity ? (
-          <h6 className="title">
-            {"Lovely developed by Hasan Kürşat Küçüköztaş"}
-          </h6>
+          <div class="footercontainer">
+            <h6 className="title">
+              {
+                "Bilgilendirme amaçlıdır, gerçeği yansıtmayabilir, sorumluluk kabul edilmez."
+              }
+            </h6>
+            <a
+              href="https://twitter.com/intent/tweet?text=Ya%C5%9Fad%C4%B1%C4%9F%C4%B1m%20%C5%9Fehrin%20risk%20grubunu%20ve%20ge%C3%A7erli%20olan%20k%C4%B1s%C4%B1tlamalar%C4%B1%20%C3%B6%C4%9Frendim.%20Sende%20%C3%B6%C4%9Frenmek%20istiyorsan%3B&url=https%3A%2F%2Fhassan1903.github.io%2Fkisitlamalar"
+              class="tweetbutton"
+            >
+              <TwitterIcon />
+              <span class="tweetlabel">Tweet</span>
+            </a>
+            <h6 className="title">
+              {"Lovely developed by Hasan Kürşat Küçüköztaş"}
+            </h6>
+          </div>
         ) : null}
       </div>
     </div>
@@ -131,6 +138,34 @@ const LightbulbSvg = (props) => {
             fill={props.fillColor}
           />
         </g>
+      </g>
+    </svg>
+  );
+};
+
+const TwitterIcon = (props) => {
+  return (
+    <svg
+      version="1.1"
+      id="Logo"
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      width="14.88px"
+      height="12.24px"
+      viewBox="0 0 248 204"
+    >
+      <g id="Logo_1_">
+        <path
+          id="blue_background"
+          fill={props.fillColor ? props.fillColor : "#fff"}
+          d="M221.95,51.29c0.15,2.17,0.15,4.34,0.15,6.53c0,66.73-50.8,143.69-143.69,143.69v-0.04
+		C50.97,201.51,24.1,193.65,1,178.83c3.99,0.48,8,0.72,12.02,0.73c22.74,0.02,44.83-7.61,62.72-21.66
+		c-21.61-0.41-40.56-14.5-47.18-35.07c7.57,1.46,15.37,1.16,22.8-0.87C27.8,117.2,10.85,96.5,10.85,72.46c0-0.22,0-0.43,0-0.64
+		c7.02,3.91,14.88,6.08,22.92,6.32C11.58,63.31,4.74,33.79,18.14,10.71c25.64,31.55,63.47,50.73,104.08,52.76
+		c-4.07-17.54,1.49-35.92,14.61-48.25c20.34-19.12,52.33-18.14,71.45,2.19c11.31-2.23,22.15-6.38,32.07-12.26
+		c-3.77,11.69-11.66,21.62-22.2,27.93c10.01-1.18,19.79-3.86,29-7.95C240.37,35.29,231.83,44.14,221.95,51.29z"
+        />
       </g>
     </svg>
   );
