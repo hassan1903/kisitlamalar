40cbd4fdf72d1982fc6092b29f3a0f3913004de1
gokhandoslu <gokhan@simpux.com>
2021-03-07 17:06:55 +0300

    fetch options mode updated to get data from https://covid19.saglik.gov.tr

--- src/index.js
+++ src/index.js
@@ -48,11 +48,11 @@ const LightBulb = () => {
   ];
   const fillColor = colorCode.find((element) => element.key === light);
   useEffect(async () => {
-    //const newEstimatedCityList = await updateEstimatedData(estimatedCityList);
-    //const newCityList = await updateData(cityList);
+    const newEstimatedCityList = await updateEstimatedData(estimatedCityList);
+    const newCityList = await updateData(cityList);
     const newRegionCode = await getRegionCode();
-    //setCityList(newCityList);
-    //setEstimatedCityList(newEstimatedCityList);
+    setCityList(newCityList);
+    setEstimatedCityList(newEstimatedCityList);
     setRegionCode(newRegionCode);
     setSpinning(false);
   }, []);
