d9c84830461cfc56bd4b7cec6bae0adaf596c7b4
Hasan Kürşat Küçüköztaş <hasan.kucukoztas@maarifa.com.tr>
2021-03-08 00:22:25 +0300

    Updates

---
+++ precache-manifest.2cfd22afc904b6f53dda5078d8cff669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7023b7a9054f027e12a",
+    "url": "/kisitlamalar/static/css/main.c395af62.chunk.css"
+  },
+  {
+    "revision": "c7023b7a9054f027e12a",
+    "url": "/kisitlamalar/static/js/main.c7023b7a.chunk.js"
+  },
+  {
+    "revision": "1a337e92f7d3492878f6",
+    "url": "/kisitlamalar/static/css/1.083fdf27.chunk.css"
+  },
+  {
+    "revision": "1a337e92f7d3492878f6",
+    "url": "/kisitlamalar/static/js/1.1a337e92.chunk.js"
+  },
+  {
+    "revision": "72502309015a7d3fa022",
+    "url": "/kisitlamalar/static/js/runtime~main.72502309.js"
+  },
+  {
+    "revision": "fa2afd02863496c3193c506dd9582279",
+    "url": "/kisitlamalar/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kisitlamalar/precache-manifest.2142ae4cf61ee8170f81e2eee4d81b7b.js"
+  "/kisitlamalar/precache-manifest.2cfd22afc904b6f53dda5078d8cff669.js"
 );
 
 workbox.clientsClaim();
